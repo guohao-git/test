2b5400a63e0e42d55c049a9bbb09a6c2f9d0656d
guohaozk <82998661+guohaozk@users.noreply.github.com>
2021-05-13 16:30:22 +0800

    新添加了一个方法

--- src/main/java/com/offcn/HelloWorld.java
+++ src/main/java/com/offcn/HelloWorld.java
@@ -2,6 +2,6 @@ package com.offcn;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("这是我的第一个git测试");
+        System.out.println("这是我的第一个git测试。。");
     }
 }
