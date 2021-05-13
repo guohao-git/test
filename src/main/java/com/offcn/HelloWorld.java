73a4e77a905217444c047095f512ac0b4aacc630
guohaozk <82998661+guohaozk@users.noreply.github.com>
2021-05-13 16:40:32 +0800

    最后一次尝试

--- src/main/java/com/offcn/HelloWorld.java
+++ src/main/java/com/offcn/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git测试。");
         System.out.println("更新内容");
+        System.out.println("66");
     }
 }
