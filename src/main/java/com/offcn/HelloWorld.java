d5e3db9e168e6834882397ff7c696ff5305176d6
guohaozk <82998661+guohaozk@users.noreply.github.com>
2021-05-13 16:48:46 +0800

    测试

--- src/main/java/com/offcn/HelloWorld.java
+++ src/main/java/com/offcn/HelloWorld.java
@@ -2,9 +2,6 @@ package com.offcn;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("这是我的第一个git测试。");
-        System.out.println("更新内容");
-        System.out.println("66");
-        System.out.println("000004");
+        System.out.println("测试");
     }
 }
